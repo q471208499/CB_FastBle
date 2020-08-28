2f0096207b411b7bf5a7e67fb42b3bddf2fdeab5
chenb <471208499@qq.com>
2020-08-28 21:11:24 +0800

    BleBaseActivity.java 优化

--- FastBleLib/src/main/java/com/clj/fastble/activity/BleBaseActivity.java
+++ FastBleLib/src/main/java/com/clj/fastble/activity/BleBaseActivity.java
@@ -3,6 +3,10 @@ package com.clj.fastble.activity;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,10 +14,11 @@ import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -25,12 +30,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cn.cb.baselibrary.activity.BaseActivity;
+import es.dmoral.toasty.MyToast;
 
 public class BleBaseActivity extends BaseActivity {
 
     private final String TAG = getClass().getSimpleName();
     private static final int REQUEST_CODE_OPEN_GPS = 2001;
     private static final int REQUEST_CODE_PERMISSION_LOCATION = 2002;
+    private final int REQUEST_ENABLE_BT = 2009;
+    private final int SCAN_TIME = 10000;//每次扫描时间
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,75 @@ public class BleBaseActivity extends BaseActivity {
                 //.setReConnectCount(1, 5000)
                 .setConnectOverTime(20000)
                 .setOperateTimeout(5000);
+    }
+
+    /**
+     * ************************************************
+     * 打开蓝牙扫描逻辑
+     * 打开蓝牙 → 要权限 → 扫描
+     * 打开蓝牙 → 有权限 → 扫描
+     * 蓝牙已开 → 要权限 → 扫描
+     * 蓝牙已开 → 有权限 → 扫描
+     * ************************************************
+     */
+    protected void goScan() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!bluetoothAdapter.isEnabled()) {
+            enableBluetooth();
+        } else if (!checkGPS()) {
+            openGPS();
+        } else {
+            if (enableBluetooth())
+                checkPermissions();
+        }
+    }
+
+    private void openGPS() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.notifyTitle)
+                .setMessage(R.string.gpsNotifyMsg)
+                .setNegativeButton(R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                MyToast.show("权限不足，请重试后允许！");
+                            }
+                        })
+                .setPositiveButton(R.string.setting,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivityForResult(intent, REQUEST_CODE_OPEN_GPS);
+                            }
+                        })
+
+                .setCancelable(false)
+                .show();
+    }
+
+    private boolean checkGPS() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkGPSIsOpen()) {
+            return false;
+        }
+        return true;
+    }
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothLeScanner leScanner;
+
+    /**
+     * 打开蓝牙
+     */
+    public boolean enableBluetooth() {
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -62,17 +139,21 @@ public class BleBaseActivity extends BaseActivity {
                     for (int i = 0; i < grantResults.length; i++) {
                         if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
                             onPermissionGranted(permissions[i]);
+                        } else {
+                            MyToast.show("权限不足，请重试后允许！");
                         }
                     }
                 }
                 break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + requestCode);
         }
     }
 
     private void onPermissionGranted(String permission) {
         switch (permission) {
             case Manifest.permission.ACCESS_FINE_LOCATION:
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !checkGPSIsOpen()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkGPSIsOpen()) {
                     new AlertDialog.Builder(this)
                             .setTitle(R.string.notifyTitle)
                             .setMessage(R.string.gpsNotifyMsg)
@@ -80,7 +161,7 @@ public class BleBaseActivity extends BaseActivity {
                                     new DialogInterface.OnClickListener() {
                                         @Override
                                         public void onClick(DialogInterface dialog, int which) {
-                                            finish();
+                                            MyToast.show("权限不足，请重试后允许！");
                                         }
                                     })
                             .setPositiveButton(R.string.setting,
@@ -97,51 +178,40 @@ public class BleBaseActivity extends BaseActivity {
                 } else {
                     setScanRule();
                     //startScan();
+                    startScanBLE();
                 }
                 break;
         }
     }
 
-    private void setScanRule() {
-        /*String[] uuids;
-        String str_uuid = et_uuid.getText().toString();
-        if (TextUtils.isEmpty(str_uuid)) {
-            uuids = null;
-        } else {
-            uuids = str_uuid.split(",");
-        }
-        UUID[] serviceUuids = null;
-        if (uuids != null && uuids.length > 0) {
-            serviceUuids = new UUID[uuids.length];
-            for (int i = 0; i < uuids.length; i++) {
-                String name = uuids[i];
-                String[] components = name.split("-");
-                if (components.length != 5) {
-                    serviceUuids[i] = null;
-                } else {
-                    serviceUuids[i] = UUID.fromString(uuids[i]);
-                }
+    private void startScanBLE() {
+        leScanner = bluetoothAdapter.getBluetoothLeScanner();
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .setReportDelay(0)
+                .build();
+        leScanner.startScan(null, settings, callback);
+        MyToast.show("开始扫描");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (leScanner != null)
+                    leScanner.stopScan(callback);
+                MyToast.show("停止扫描");
             }
-        }
+        }, SCAN_TIME);
+    }
 
-        String[] names;
-        String str_name = et_name.getText().toString();
-        if (TextUtils.isEmpty(str_name)) {
-            names = null;
-        } else {
-            names = str_name.split(",");
-        }
+    protected ScanCallback callback;
 
-        String mac = et_mac.getText().toString();
-
-        boolean isAutoConnect = sw_auto.isChecked();*/
-
+    private void setScanRule() {
         BleScanRuleConfig scanRuleConfig = new BleScanRuleConfig.Builder()
                 //.setServiceUuids(serviceUuids)      // 只扫描指定的服务的设备，可选
                 //.setDeviceName(true, names)   // 只扫描指定广播名的设备，可选
                 //.setDeviceMac(mac)                  // 只扫描指定mac的设备，可选
                 //.setAutoConnect(isAutoConnect)      // 连接时的autoConnect参数，可选，默认false
-                .setScanTimeOut(0)              // 扫描超时时间，可选，默认10秒
+                //.setScanTimeOut(0)              // 扫描超时时间，可选，默认10秒
                 .build();
         BleManager.getInstance().initScanRule(scanRuleConfig);
     }
@@ -154,12 +224,6 @@ public class BleBaseActivity extends BaseActivity {
     }
 
     private void checkPermissions() {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!bluetoothAdapter.isEnabled()) {
-            Toast.makeText(this, getString(R.string.please_open_blue), Toast.LENGTH_LONG).show();
-            return;
-        }
-
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
         List<String> permissionDeniedList = new ArrayList<>();
         for (String permission : permissions) {
@@ -176,5 +240,17 @@ public class BleBaseActivity extends BaseActivity {
         }
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (!checkGPS()) {
+                openGPS();
+            } else {
+                checkPermissions();
+            }
+        } else if (requestCode == REQUEST_CODE_OPEN_GPS) {
+            checkPermissions();
+        }
+    }
 }
