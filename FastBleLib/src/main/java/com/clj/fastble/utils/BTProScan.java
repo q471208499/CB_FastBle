e9c988223f0c214e52243e9f515a7ea3072687fd
chen <471208499@qq.com>
2021-03-19 17:33:25 +0800

    BTProScan 逻辑严谨

--- FastBleLib/src/main/java/com/clj/fastble/utils/BTProScan.java
+++ FastBleLib/src/main/java/com/clj/fastble/utils/BTProScan.java
@@ -1,7 +1,5 @@
 package com.clj.fastble.utils;
 
-import android.util.Log;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -78,33 +76,37 @@ public class BTProScan {
     }
 
     public boolean verify() {
-        if (adHex == null || adHex.trim().isEmpty()) return false;//字符串异常
-        adHex = adHex.replace(" ", "");
+        try {
+            if (adHex == null || adHex.trim().isEmpty()) return false;//字符串异常
+            adHex = adHex.replace(" ", "");
 
-        if (adHex.contains(HEX_HEAD) && adHex.contains(HEX_END)) {//校验包头
-            if (adHex.indexOf(HEX_HEAD) - 2 > -1)
-                adHex = adHex.substring(adHex.indexOf(HEX_HEAD) - 2);
-            else
+            if (adHex.contains(HEX_HEAD) && adHex.contains(HEX_END)) {//校验包头
+                if (adHex.indexOf(HEX_HEAD) - 2 > -1)
+                    adHex = adHex.substring(adHex.indexOf(HEX_HEAD) - 2);
+                else
+                    return false;
+            } else
                 return false;
-        } else
-            return false;
 
-        int lengthCal = Integer.valueOf(adHex.substring(0, 2), RADIX_16);
-        if (adHex.length() < (lengthCal + 1) * 2) return false;// 长度 不匹配
-        adHex = adHex.substring(0, (lengthCal + 1) * 2);
-        Log.i(TAG, "verify: " + adHex);
+            int lengthCal = Integer.valueOf(adHex.substring(0, 2), RADIX_16);
+            if (adHex.length() < (lengthCal + 1) * 2) return false;// 长度 不匹配
+            adHex = adHex.substring(0, (lengthCal + 1) * 2);
+            System.out.println(TAG + "  verify: " + adHex);
 
-        String head = adHex.substring(2, 4);
-        String end = adHex.substring(adHex.length() - 2);
-        if (!HEX_HEAD.equals(head) || !HEX_END.equals(end)) return false;//标识头/尾 不匹配
+            String head = adHex.substring(2, 4);
+            String end = adHex.substring(adHex.length() - 2);
+            if (!HEX_HEAD.equals(head) || !HEX_END.equals(end)) return false;//标识头/尾 不匹配
 
-        String csHex = adHex.substring(adHex.length() - 4, adHex.length() - 2);
-        String toCalHex = adHex.substring(2, adHex.length() - 4);
-        int csCalInt = calCs(toCalHex);
+            String csHex = adHex.substring(adHex.length() - 4, adHex.length() - 2);
+            String toCalHex = adHex.substring(2, adHex.length() - 4);
+            int csCalInt = calCs(toCalHex);
 
-        if (Integer.valueOf(csHex, RADIX_16) == csCalInt % 256) {
-            packageDada();
-            return true;
+            if (Integer.valueOf(csHex, RADIX_16) == csCalInt % 256) {
+                packageDada();
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return false;
     }
@@ -123,40 +125,44 @@ public class BTProScan {
     }
 
     private void packageDada() {
-        String cmdHex = adHex.substring(4, 6);
-        String time = HexUtil.bigOrSmallEndian(adHex.substring(6, 10));
-        String flowHex = HexUtil.bigOrSmallEndian(adHex.substring(10, 18));
-        double flow = Long.valueOf(flowHex, RADIX_16) / 1000d;
-        String contraryHex = HexUtil.bigOrSmallEndian(adHex.substring(18, 26));
-        double contrary = Long.valueOf(contraryHex, RADIX_16) / 1000d;
-        String instantaneousHex = HexUtil.bigOrSmallEndian(adHex.substring(26, 30));
-        long instantaneous = Long.valueOf(instantaneousHex, RADIX_16);
-        String usageTimeHex = HexUtil.bigOrSmallEndian(adHex.substring(30, 34));
-        long usageTime = Long.valueOf(usageTimeHex, RADIX_16);
-        String voltageHex = HexUtil.bigOrSmallEndian(adHex.substring(34, 36));
-        double voltage = (Integer.parseInt(voltageHex, RADIX_16) + 250) / 100d;
-        String warningHex = adHex.substring(36, 38);
-        String warningStr = Integer.toBinaryString(Integer.parseInt(warningHex, 16));
-        String warnings = warningStr.length() > 4 ? warningStr.substring(0, warningStr.length() - 4) : "0";
-        warnings = String.format("%04d", Integer.parseInt(warnings));
-        int flowStatus = Integer.parseInt(warnings.substring(0, 1));
-        int timeStatus = Integer.parseInt(warnings.substring(1, 2));
-        int magStatus = Integer.parseInt(warnings.substring(2, 3));
-        int voltageStatus = Integer.parseInt(warnings.substring(3, 4));
-        int signal = Integer.parseInt(warningHex, 16) % 16;
-        dataMap.put(DATA_KEY_CMD, cmdHex);
-        dataMap.put(DATA_KEY_TIME, time);
-        dataMap.put(DATA_KEY_FLOW, flow);
-        dataMap.put(DATA_KEY_CONTRARY, contrary);
-        dataMap.put(DATA_KEY_INSTANTANEOUS, instantaneous);
-        dataMap.put(DATA_KEY_USAGE_TIME, usageTime);
-        dataMap.put(DATA_KEY_VOLTAGE, voltage);
-        dataMap.put(DATA_KEY_WARNINGS, warnings);
-        dataMap.put(DATA_KEY_FLOW_STATUS, flowStatus);
-        dataMap.put(DATA_KEY_TIME_STATUS, timeStatus);
-        dataMap.put(DATA_KEY_MAG_STATUS, magStatus);
-        dataMap.put(DATA_KEY_VOLTAGE_STATUS, voltageStatus);
-        dataMap.put(DATA_KEY_SIGNAL, signal);
+        try {
+            String cmdHex = adHex.substring(4, 6);
+            String time = HexUtil.bigOrSmallEndian(adHex.substring(6, 10));
+            String flowHex = HexUtil.bigOrSmallEndian(adHex.substring(10, 18));
+            double flow = Long.valueOf(flowHex, RADIX_16) / 1000d;
+            String contraryHex = HexUtil.bigOrSmallEndian(adHex.substring(18, 26));
+            double contrary = Long.valueOf(contraryHex, RADIX_16) / 1000d;
+            String instantaneousHex = HexUtil.bigOrSmallEndian(adHex.substring(26, 30));
+            long instantaneous = Long.valueOf(instantaneousHex, RADIX_16);
+            String usageTimeHex = HexUtil.bigOrSmallEndian(adHex.substring(30, 34));
+            long usageTime = Long.valueOf(usageTimeHex, RADIX_16);
+            String voltageHex = HexUtil.bigOrSmallEndian(adHex.substring(34, 36));
+            double voltage = (Integer.parseInt(voltageHex, RADIX_16) + 250) / 100d;
+            String warningHex = adHex.substring(36, 38);
+            String warningStr = Integer.toBinaryString(Integer.parseInt(warningHex, 16));
+            String warnings = warningStr.length() > 4 ? warningStr.substring(0, warningStr.length() - 4) : "0";
+            warnings = String.format("%04d", Integer.parseInt(warnings));
+            int flowStatus = Integer.parseInt(warnings.substring(0, 1));
+            int timeStatus = Integer.parseInt(warnings.substring(1, 2));
+            int magStatus = Integer.parseInt(warnings.substring(2, 3));
+            int voltageStatus = Integer.parseInt(warnings.substring(3, 4));
+            int signal = Integer.parseInt(warningHex, 16) % 16;
+            dataMap.put(DATA_KEY_CMD, cmdHex);
+            dataMap.put(DATA_KEY_TIME, time);
+            dataMap.put(DATA_KEY_FLOW, flow);
+            dataMap.put(DATA_KEY_CONTRARY, contrary);
+            dataMap.put(DATA_KEY_INSTANTANEOUS, instantaneous);
+            dataMap.put(DATA_KEY_USAGE_TIME, usageTime);
+            dataMap.put(DATA_KEY_VOLTAGE, voltage);
+            dataMap.put(DATA_KEY_WARNINGS, warnings);
+            dataMap.put(DATA_KEY_FLOW_STATUS, flowStatus);
+            dataMap.put(DATA_KEY_TIME_STATUS, timeStatus);
+            dataMap.put(DATA_KEY_MAG_STATUS, magStatus);
+            dataMap.put(DATA_KEY_VOLTAGE_STATUS, voltageStatus);
+            dataMap.put(DATA_KEY_SIGNAL, signal);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public Map<String, Object> getDataMap() {
@@ -255,7 +261,7 @@ public class BTProScan {
     }
 
     public static void main(String[] args) {
-        String hex = "AAA136801007874D75D5E5098CEB7AEA42E78004C16AAA";
+        String hex = "14 68 01 30 17 B5 F3 C0 08 3E 1C 48 AC 00 00 00 00 78 00 E6 16 ";
         BTProScan proScan = new BTProScan(hex);
         if (proScan.verify()) {
             System.out.println(proScan.getDataMap());
